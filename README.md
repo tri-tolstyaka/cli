aef1dbf821c7930169011956a14d18c575a7d7de
Shuvani <gnomik2311@mail.ru>
2021-09-30 19:40:39 +0300

    put init in independent fie

--- README.md
+++ README.md
@@ -1,23 +1,16 @@
-# tolst-cli
-This is the platform for the microfrontends
+# @tri-tolstiaka/cli
+This is the platform for the microfrontends.
 
-## How to check that it works?
-Into your project directory run:
-```
-npx @tri-tolstiaka/cli --init
-npm install .
-npx @tri-tolstiaka/cli --server
-```
+# How to use?
+1.
 
-## How to publish?
-You can publish new npm version only from the main branch!
 ```
-git add .
-git commit -m "-"
-npm version patch
-// можно посмотреть появился ли тег в git graph
-npm run new:version
+npm install @tri-tolstiaka/cli
 ```
 
-## How to use?
-See the example project - https://github.com/Shuvani/tolst-dummy
+2. 
+
+```
+npx tolst-cli --help // check available actions with descriptions
+```
+
---
+++ src/init.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path')
+const childProcess = require('child_process')
+const packagePath = path.resolve('package.json') // find relative to the place where process was started
+const tsConfigPath = path.resolve('tsconfig.json');
+
+
+const init = () => {
+    const dependencies = [
+        '@tri-tolstiaka/dev-server',
+        'react@17.0.2',
+        'react-dom@17.0.2',
+        'typescript',
+        'ts-loader'
+    ]
+    const devDeps = [
+        '@types/react',
+        '@types/react-dom'
+    ]
+    childProcess
+        .execSync(
+            `npm install --save ${dependencies.join(' ')}`,
+            { stdio: 'inherit' }
+        )
+    childProcess
+        .execSync(
+            `npm install --save-dev ${devDeps.join(' ')} @tri-tolstiaka/dev-server`,
+            { stdio: 'inherit' }
+        )
+
+    const package = require(packagePath)
+    package.scripts = package.scripts || {}
+    package.scripts.tolst_help = 'npx tolst-cli --help';
+    package.scripts.tolst_init = 'npx tolst-cli --init';
+    package.scripts.start = 'tolst-cli --server';
+
+    if (!fs.existsSync(tsConfigPath)) {
+        const isConfig = {
+            "compilerOptions": {
+                "target": "es6",
+                "module": "esnext",
+                "lib": ["DOM", "ES2021"],
+                "jsx": "react",
+                "strict": true,
+                "noImplicitAny": false,
+                "esModuleInterop": true,
+                "skipLibCheck": true,
+                "forceConsistentCasingInFileNames": true,
+                "moduleResolution": "node",
+            },
+            "exclude": [
+                "node_modules",
+                "**/*.test.ts",
+                "**/*.test.tsx",
+                "node_modules/@types/jest"
+            ]
+        }
+        fs.writeFileSync(packagePath, JSON.stringify(isConfig, null, 4))
+    }
+    fs.writeFileSync(packagePath, JSON.stringify(package, null, 4) + '\n')
+}
+
+module.exports = init;
